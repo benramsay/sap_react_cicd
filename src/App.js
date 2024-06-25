b36add7d0370db1574c67da9cbab7376519d9a28
Ramzi <ramzi.programming@gmail.com>
2024-06-25 14:31:51 +0200

    Changing application

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br />
+        <br />
+        <h1>Just for test</h1>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h3>React.js Project</h3>
         </a>
       </header>
     </div>
